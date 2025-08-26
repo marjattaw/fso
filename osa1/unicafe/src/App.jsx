99597288fc9c995c859d961838c35cae03bc208d
Marjatta Wallinheimo <marjatta.wallinheimo@gmail.com>
2025-08-26 10:32:33 +0300

    Tehtävä 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallentaa napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
 
   return (
     <div>
@@ -13,10 +16,13 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutraali</button>
       <button onClick={() => setBad(bad + 1)}>huono</button>
 
-      <h2> Statistiikka</h2>
-      <p> Hyvä {good}</p>
-      <p> Neutraali {neutral}</p>
-      <p> Huono {bad}</p>
+      <h2>Statistiikka</h2>
+      <p>Hyvä {good}</p>
+      <p>Neutraali {neutral}</p>
+      <p>Huono {bad}</p>
+      <p>Kaikki {all}</p>
+      <p>Keskiarvo {average.toFixed(1)}</p>
+      <p>Positiivisia {positive.toFixed(1)} %</p>
     </div>
   )
 }

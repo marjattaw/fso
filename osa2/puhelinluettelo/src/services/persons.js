9a7508ea7788fb89cb16667610a80f32fa1877ca
Marjatta Wallinheimo <marjatta.wallinheimo@gmail.com>
2025-09-16 11:02:49 +0300

    Lisäys

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+// DEV (kun frontti ja backend eri porteissa, esim. 5173 & 3001):
+// const baseUrl = 'http://localhost:3001/api/persons'
+
+// PROD (kun frontti palvellaan samasta hostista kuin backend, esim. Render):
+const baseUrl = '/api/persons'
 
 const getAll = () =>
   axios.get(baseUrl).then(res => res.data)
@@ -8,10 +12,11 @@ const getAll = () =>
 const create = (newPerson) =>
   axios.post(baseUrl, newPerson).then(res => res.data)
 
+// 3.17 asti ei käytössä – älä kutsu tätä (valmiina myöhemmin)
 const update = (id, updatedPerson) =>
   axios.put(`${baseUrl}/${id}`, updatedPerson).then(res => res.data)
 
 const remove = (id) =>
-  axios.delete(`${baseUrl}/${id}`)
+  axios.delete(`${baseUrl}/${id}`).then(res => res.data)
 
 export default { getAll, create, update, remove }
